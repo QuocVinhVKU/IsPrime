efa6692130d4f0e61718e918b466ad773666fef7
QuocVinhVKU <kuroblackshirowhile@gmail.com>
2023-05-21 10:45:18 +0700

    create IsPrime

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 2)
+                Console.WriteLine(number + " is not a prime");
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                    Console.WriteLine(number + " is a prime");
+                else
+                    Console.WriteLine(number + " is not a prime");
+            }
+            Console.ReadKey();
+        }
+    }
+}
